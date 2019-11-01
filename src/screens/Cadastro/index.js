b6a2b6fb06b17eda5ff862f5696471f7ae470d00
jpdalbozp <joao.dalbo@zephyrplace.com>
2019-11-01 13:53:42 -0300

    Foto no cadastro 1

--- src/screens/Cadastro/index.js
+++ src/screens/Cadastro/index.js
@@ -1,36 +1,121 @@
 import React from 'react';
-import { KeyboardAvoidingView, View, TextInput } from 'react-native'
+import { KeyboardAvoidingView, View, TextInput, TouchableOpacity, Alert, Image, Text } from 'react-native'
 
 import styles from './styles'
 
-import Background from '../../components/Background'
+import { Background } from '../../components/Background'
 import Button from '../../components/Button'
 import Input from '../../components/Input'
 
-export default function Cadastro(props) {
-    return (
-        <Background>
-            <KeyboardAvoidingView style={styles.content}>
+import Constants from 'expo-constants'
+import * as Permissions from 'expo-permissions';
+import * as ImagePicker from 'expo-image-picker';
 
-                
-                <Input placeholder='Digite seu nome' />
+import Plus from 'react-native-vector-icons/Feather'
+import perfil from '../../assets/perfil.jpg'
 
-                <Input placeholder='Digite seu sobrenome' />
+export default function Perfil(props) {
+    const [avatar, setAvatar] = useState('')
 
-                <Input placeholder='Digite seu CPF' />
+    useEffect(() => {
+        getPermissions()
+    }, [])
 
-                <Input placeholder='Digite o tipo do serviço' />
+    async function getPermissions() {
+        if (Constants.platform.ios) {
+            const { status } = await Permissions.askAsync(Permissions.CAMERA_ROLL)
+            if (status !== 'granted') {
+                console.log('Precisamos desta permissão para tudo funcionar corretamente')
+            }
+        }
+    }
+    async function pickImage() {
+        var result = await ImagePicker.launchImageLibraryAsync({
+            mediaTypes: ImagePicker.MediaTypeOptions.All,
+            allowsEditing: true,
+            aspect: [4, 3]
+        })
+        if (result.cancelled === false) {
+            setAvatar(result.uri)
+        }
 
-                <View style={{ flexDirection: "row" }}>
-                    <Button 
-                        texto='Cadastrar' 
-                        style={styles.buttonRight}/>
-                    <Button 
-                        texto='Voltar' 
-                        style={styles.buttonLeft}
-                        onPress={() => props.navigation.goBack()} />
+        setAvatar(result.uri)
+    }
+    async function pickImageWithCamera() {
+        var result = await ImagePicker.launchCameraAsync({
+            mediaTypes: ImagePicker.MediaTypeOptions.All,
+            allowsEditing: true,
+            aspect: [4, 3]
+        })
+
+        if (result.cancelled === false) {
+            setAvatar(result.uri)
+        }
+
+        setAvatar(result.uri)
+    }
+    function deletePick() {
+        setAvatar('')
+    }
+
+    function imagem() {
+        console.log(avatar)
+        Alert.alert(
+            'Mensagem',
+            'Você deseja utilizar a câmera ou a galeria?',
+            [{ text: 'Excluir', onPress: () => deletePick() },
+            { text: 'Câmera', onPress: () => pickImageWithCamera() },
+            { text: 'Galeria', onPress: () => pickImage() }]
+        )
+    }
+    export default function Cadastro(props) {
+        return (
+            <Background>
+                <View style={styles.viewImage}>
+                    <Image
+                        source={avatar ? { uri: avatar } : perfil}
+                        style={styles.image}
+                    />
+                    <TouchableOpacity style={styles.circle} onPress={() => imagem()}>
+                        <Plus name='plus' size={20} color='#fff' />
+                    </TouchableOpacity>
                 </View>
-            </KeyboardAvoidingView>
-        </Background>
-    )
+                <KeyboardAvoidingView style={{ flex: 0.7, width: '100%', alignItems: 'center', justifyContent: 'space-evenly' }}>
+                    <Input placeholder="Nome completo" />
+                    <Input placeholder="Digite seu sobrenome" />
+                    <Input placeholder="Mais informações" />
+                    <View style={{ flexDirection: "row" }}>
+                        <Button
+                            texto='Salvar'
+                            style={styles.buttonRight} />
+                        <Button
+                            texto='Voltar'
+                            style={styles.buttonLeft}
+                            onPress={() => props.navigation.goBack()} />
+                    </View>
+                </KeyboardAvoidingView>
+                <KeyboardAvoidingView style={styles.content}>
+
+
+                    <Input placeholder='Digite seu nome' />
+
+                    <Input placeholder='Digite seu sobrenome' />
+
+                    <Input placeholder='Digite seu CPF' />
+
+                    <Input placeholder='Digite o tipo do serviço' />
+
+                    <View style={{ flexDirection: "row" }}>
+                        <Button
+                            texto='Cadastrar'
+                            style={styles.buttonRight} />
+                        <Button
+                            texto='Voltar'
+                            style={styles.buttonLeft}
+                            onPress={() => props.navigation.goBack()} />
+                    </View>
+                </KeyboardAvoidingView>
+            </Background>
+        )
+    }
 }
