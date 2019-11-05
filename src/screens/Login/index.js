69bc8716dfb36a0d3d6a1ea337d99bdef888f33e
danilozp <danilo.lopes@zephyrplace.com>
2019-11-05 14:32:36 -0300

    troca de cores

--- src/screens/Login/index.js
+++ src/screens/Login/index.js
@@ -31,7 +31,7 @@ export default function Login(props) {
                             onPress={() => props.navigation.navigate('Cadastro')} />
                     </View>
 
-                    <Link text='Perfil' onPress={() => props.navigation.navigate('Perfil')} />
+                    {/* <Link text='Perfil' onPress={() => props.navigation.navigate('Perfil')} /> */}
 
                 </KeyboardAvoidingView>
             </View>
