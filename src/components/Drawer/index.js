e471f92f101eb0f8821d1b706ba74b63f4557d5f
danilozp <danilo.lopes@zephyrplace.com>
2019-11-05 15:29:15 -0300

    considerações finais Front-End

--- src/components/Drawer/index.js
+++ src/components/Drawer/index.js
@@ -1,23 +1,33 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { View, Dimensions, TouchableOpacity, Text } from 'react-native';
-import { white } from 'ansi-colors';
+import DrawerLayout from 'react-native-gesture-handler/DrawerLayout';
+
+// import Icon from 'react-native-vector-icons/MaterialIcons'
+// import { Icon } from 'native-base';
+// import { white } from 'ansi-colors';
 
 const { width, height } = Dimensions.get('screen')
 
 export default function Drawer({ navigation }) {
 
   return (
-    <View style={{
-      width: width / 2,
-      height,
-      backgroundColor: '#101056'
-    }}>
-      <TouchableOpacity style={{ paddingTop: 20, width: 280, height: 20 }}
-        onPress={() => navigation.navigate('Perfil')}>
-        <Text style={{ color: 'white', justifyContent: 'center', alignItems: 'center', fontSize: 18 }}>
-          Perfil
-        </Text>
-      </TouchableOpacity>
-    </View>
+
+
+     <View style={{
+       flexDirection: 'row',
+       width: 280,
+       height,
+       backgroundColor: '#101056'
+     }}>
+       <TouchableOpacity style={{ paddingTop: 20, width: 280, height: 20 }}
+         onPress={() => navigation.navigate('Perfil')}>
+         {/* <Icon name='Perfil' size={25} color='white'>
+           <Text>Perfil</Text>
+         </Icon> */}
+         <Text style={{ color: 'white', fontSize: 18, paddingLeft: 15 }}>
+           Perfil
+         </Text>
+       </TouchableOpacity>
+     </View>
   );
 }
