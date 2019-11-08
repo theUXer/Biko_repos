84f7954389afbbcb95080867c25bf3bf02189392
danilozp <danilo.lopes@zephyrplace.com>
2019-11-08 13:54:28 -0300

    info-edit

--- src/screens/Info/index.js
+++ src/screens/Info/index.js
@@ -1,13 +1,21 @@
 import React from 'react'
 import { View, Image, Text } from 'react-native';
-import { Logo } from '../../helpers/Images'
+import { Logo, Bg } from '../../helpers/Images'
 import styles from '../Perfil/styes'
+import { Background } from '../../components/Background'
+
+
 export default function Info(props) {
-    return <View style={{ flex: 1, justifyContent: 'center', alignItems:'center' }}>
-        <Image
-            source={Logo}
-            style={styles.image}
-        />
-        <Text style={{ fontSize: 25 }}>Nós somos João e Danilo</Text>
+    return (
+    
+        <Background source={Bg} resizeMode='cover'>
+    
+    <View style={{ flex: 1, justifyContent: 'center', alignItems:'center' }}>
+        <Text style={{ fontSize: 25, color: 'white', alignItems: 'center' }}>
+            
+            Esta aplicação foi desenvolvida pelos alunos: - Danilo Ferreira - João Dalbo.</Text>
     </View>
-}
+
+    </Background>
+
+);}
