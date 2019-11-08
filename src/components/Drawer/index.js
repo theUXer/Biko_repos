4ba31a0013ff5ee961887efb4dacf49e3f16895c
jpdalbozp <joao.dalbo@zephyrplace.com>
2019-11-08 13:52:51 -0300

    Drawer finalizada

--- src/components/Drawer/index.js
+++ src/components/Drawer/index.js
@@ -3,6 +3,7 @@ import { View, Dimensions, TouchableOpacity, Text } from 'react-native';
 import { bikoColor } from '../../helpers/Colors'
 import DrawerLayout from 'react-native-gesture-handler/DrawerLayout';
 import { setRecoveryProps } from 'expo/build/ErrorRecovery/ErrorRecovery';
+import { white } from 'ansi-colors';
 
 // import Icon from 'react-native-vector-icons/MaterialIcons'
 // import { Icon } from 'native-base';
@@ -12,43 +13,52 @@ const { width, height } = Dimensions.get('screen')
 
 export default function Drawer({ navigation }) {
 
-  
+
   return (
 
-    <View style={{ flex: 1, justifyContent: 'space-between', 
-    backgroundColor: '#101056'  }}>
-     <View style={{
-       
-       width: 280,
-       
-       backgroundColor: '#101056'
-     }}>
-       <TouchableOpacity style={{ marginTop: 20, width: 280, height: 50, backgroundColor: 'white' }}
-         onPress={() => navigation.navigate('Perfil')}>
-         <Text style={{ color: bikoColor.primary, fontSize: 18, paddingLeft: 15, marginTop: 10 }}>
-           Perfil
+    <View style={{
+      flex: 1, justifyContent: 'space-between',
+      backgroundColor: '#101056'
+    }}>
+      <View style={{
+
+        width: 280,
+
+        backgroundColor: '#101056'
+      }}>
+        <TouchableOpacity style={{ marginTop: 20, width: 280, height: 50, backgroundColor: 'white' }}
+          onPress={() => navigation.navigate('Perfil')}>
+          <Text style={{ color: bikoColor.primary, fontSize: 18, paddingLeft: 15, marginTop: 10 }}>
+            Perfil
          </Text>
-       </TouchableOpacity>
+        </TouchableOpacity>
 
-       <TouchableOpacity style={{ marginTop: 5, width: 280, height: 50, backgroundColor: bikoColor.primary }}
-         onPress={() => navigation.navigate('Info')}>
-         <Text style={{ color: 'white', fontSize: 18, paddingLeft: 15, marginTop: 10 }}>
-           Sobre
+        <TouchableOpacity style={{ marginTop: 5, width: 280, height: 50, backgroundColor: bikoColor.primary }}
+          onPress={() => navigation.navigate('Info')}>
+          <Text style={{ color: 'white', fontSize: 18, paddingLeft: 15, marginTop: 10 }}>
+            Sobre
          </Text>
-       </TouchableOpacity>
+        </TouchableOpacity>
 
-       
-     </View>
+        <TouchableOpacity style={{ marginTop: 5, width: 280, height: 50, backgroundColor: 'white' }}
+          onPress={() => navigation.navigate('Home')}>
+          <Text style={{ color: bikoColor.primary, fontSize: 18, paddingLeft: 15, marginTop: 10 }}>
+            Home
+         </Text>
+        </TouchableOpacity>
 
-  
+
+      </View>
+
+
 
       <TouchableOpacity style={{ width: 280, height: 50 }}
-         onPress={() => navigation.navigate('Login')}>
-         <Text style={{ color: 'white', fontSize: 18, paddingLeft: 15, textAlign: 'center' }}>
-           Sair
+        onPress={() => navigation.navigate('Login')}>
+        <Text style={{ color: 'white', fontSize: 18, paddingLeft: 15, textAlign: 'center' }}>
+          Sair
          </Text>
-       </TouchableOpacity>
+      </TouchableOpacity>
 
-     </View>
+    </View>
   );
 }
